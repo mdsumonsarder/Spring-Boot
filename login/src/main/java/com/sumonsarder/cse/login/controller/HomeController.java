eea3dad2c956ed703ebdfa5a3f9bde9944144dde
Md Sumon Sarder <sumon@innoweb.co>
2022-08-10 14:40:53 +0600

    Spring Security

---
+++ login/src/main/java/com/sumonsarder/cse/login/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.sumonsarder.cse.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/","/home"})
+    public String homePage(Model model){
+        model.addAttribute("message","Md. Sumon Sarder");
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String helloUser(){
+        return "helloUser";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
---
+++ login/src/test/java/com/sumonsarder/cse/login/HomeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumonsarder.cse.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
